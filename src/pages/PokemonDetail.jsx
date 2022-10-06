8f03f04dc136b1bc834258e58a83f7cefc3713ac
Cristian Venegas <cristian.venegas@materoceano.com>
2022-10-06 15:00:01 -0300

    add : useParams()

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const PokemonDetail = () => {
+  const {name} = useParams()
   return (
     <div>
       <h1>Lista detalle</h1>
-      
+      <p>nombre : {name}</p>
       <p>Imagen :</p>
       <p>Estadistica:</p>
       <p>tipo</p>
