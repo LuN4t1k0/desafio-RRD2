eefee8afab2ab68a0869a91ccb02f09763c03d33
Cristian Venegas <cristian.venegas@materoceano.com>
2022-10-08 00:28:31 -0300

    add : homePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import MyButton from "../components/MyButton";
-import principal from '../assets/img/imgPrincipal.png'
+import principal from "../assets/img/imgPrincipal.png";
 const Home = () => {
   const searchPokemonByName = (e) => {
     e.preventDefault();
     navigate(`${nombre}`);
   };
   return (
-    <div className="h-[calc(100vh-60px)] w-full bg-green-500 ">
-     
-      {/* <div className="bg-red-500  flex justify-center">
+    <div className="h-[calc(100vh-60px)] w-full bg-red-500 flex flex-col items-center p-20">
+      
+
+      <div className="pt-6">
         <img src={principal} alt="principal" />
-      </div> */}
+      </div>
+      <div>
+        {/* <h1 className="text-2xl pt-6 text-white ">Bienvenido Maestro Pokemon</h1> */}
+      </div>
     </div>
+
+    
   );
 };
 
 export default Home;
 
-{/* <div className="sm:bg-black bg-cyan-500 flex flex-col h-[calc(100vh-80px)] w-screen  "></div> */}
+{
+  /* <div className="sm:bg-black bg-cyan-500 flex flex-col h-[calc(100vh-80px)] w-screen  "></div> */
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='w-screem h-screen justify-center'>
+      <img src="" alt="" />
+      <h1>:( Not Found</h1>
+      </div>
+  )
+}
+
+export default NotFound
