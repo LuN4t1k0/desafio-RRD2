0bb920e6213f699cd28f155daf77a0aee7a4ee3b
Cristian Venegas <cristian.venegas@materoceano.com>
2022-10-08 01:24:32 -0300

    bug: className recibe true

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const MyNavBar = () => {
@@ -7,40 +6,39 @@ const MyNavBar = () => {
   };
 
   return (
-    <div className="w-full h-[64px] bg-red-500 shadow text-white flex justify-between items-center">
-
-    <div className="izquierda flex items-center">
-    <div className>
-      <img className="h-10 pl-4 pr-4 " src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png" alt="" />
-    </div>
-    <div className="text font-semibold">PokeAPI</div>
-    </div>
+    <div className="w-full h-[64px] bg-red-500 shadow text-white flex items-center">
+      <div className="izquierda flex items-center">
+        <div>
+          <img
+            className="h-10 pl-4 pr-4"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png"
+            alt="pikachu"
+          />
+        </div>
+        <div className="text font-semibold">PokeAPI</div>
+      </div>
 
       <div className="izquierda w-1/3 lg:w-60 md:w-60">
         <div className="logo w-full flex items-center justify-center  ">
           <h1 className="text-lg pr-2">
             <i className="fa-solid fa-camera"></i>
           </h1>
-          <div className="imgLogo">
-            
-          </div>
+          <div className="imgLogo"></div>
         </div>
       </div>
-      <div className="derecha flex justify-around w-60 ">
-        <div className="home  ">
-          <NavLink className="text-lg pl-" end to="/">
+      <div className="flex justify-around w-60 ">
+        <div>
+          <NavLink style={setActiveClass} className="text-lg pl-4" end to="/">
             HOME
           </NavLink>
         </div>
-        <div className="pokemon">
-          <NavLink className="text-lg" to="/pokemon">
+        <div>
+          <NavLink style={setActiveClass} className="text-lg" to="/pokemon">
             POKEMON
           </NavLink>
         </div>
       </div>
     </div>
-
-    
   );
 };
 
--- src/pages/SearchPokemon.jsx
+++ src/pages/SearchPokemon.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 import MyButton from "../components/MyButton";
 import { AppContext } from "../context/AppContext";
 
+
 const SearchPokemon = () => {
   const [nombre, setNombre] = useState("");
   const { data } = useContext(AppContext);
   const navigate = useNavigate();
- 
 
   const handleSearch = (e) => {
     const pokeName = e.target.value;
@@ -25,16 +25,28 @@ const SearchPokemon = () => {
 
   return (
     <>
-      <select onChange={handleSearch} name="" id="">
-        <option value="">Selecciona un Pokemon</option>
-        {data.map((item) => (
-          <option key={item.name} value={item.name}>
-            {item.name}
-          </option>
-        ))}
-      </select>
+      <div className="flex flex-col items-center align-middle justify-center h-[calc(100vh-60px)] w-full bg-slate-500">
+        <div className="ok">
+          <img src="" alt="" />
+        </div>
 
-      <MyButton search={searchPokemonByName} text="Buscar Pokemon" />
+        <div>
+          <select
+            className="h-20 text-xl"
+            onChange={handleSearch}
+            name=""
+            id=""
+          >
+            <option value="">Selecciona un Pokemon</option>
+            {data.map((item) => (
+              <option key={item.name} value={item.name}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <MyButton search={searchPokemonByName} text="Buscar Pokemon" />
+      </div>
     </>
   );
 };
